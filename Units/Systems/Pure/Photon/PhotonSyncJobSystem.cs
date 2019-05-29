2fe90bff9dd02f65e7d16e6cab33de4561911967
xsindrom <xsindrom.force@gmail.com>
2019-05-29 22:22:54 +0300

    Handy photon sync movement system using PureECS

---
+++ Units/Systems/Pure/Photon/PhotonSyncJobSystem.cs
@@ -0,0 +1,49 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+//using Unity.Burst;
+using Photon.Pun;
+namespace Units.PureECS
+{
+    public class PhotonSyncJobSystem : JobComponentSystem
+    {
+        //[BurstCompile]
+        struct PhotonSyncJobHandler : IJobProcessComponentData<PhotonSyncTransform, Position, Rotation>
+        {
+            public float serializationRate;
+            public double time;
+            public double serverTime;
+
+            public void Execute([ReadOnly] ref PhotonSyncTransform transform,  ref Position position, ref Rotation rotation)
+            {
+                double lag = math.abs(time - transform.infoTime);
+                float3 networkPosition = transform.networkPosition + transform.direction * (float)lag;
+                float distance = math.distance(position.Value, transform.networkPosition);
+
+                //Position
+                float3 toVector = transform.networkPosition - position.Value;
+                float mag = math.sqrt(toVector.x * toVector.x + toVector.y * toVector.y + toVector.z * toVector.z);
+
+                position.Value = mag == 0 ? position.Value : position.Value + toVector / mag * (distance / serializationRate);
+                ///Rotation
+                float dot = math.dot(rotation.Value, transform.networkRotation);
+                float angle = dot > 1.0f - 0.000001f ? 0.0f : math.degrees(math.acos(math.min(math.abs(dot), 1.0f)) * 2.0f);
+
+                rotation.Value = angle == 0 ? transform.networkRotation : math.slerp(rotation.Value, transform.networkRotation, math.min(1.0f, angle / serializationRate));
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var syncJob = new PhotonSyncJobHandler()
+            {
+                serializationRate = PhotonNetwork.SerializationRate,
+                time = PhotonNetwork.Time
+            };
+            return syncJob.Schedule(this, inputDeps);
+        }
+
+    }
+}
---
+++ Units/Systems/Pure/Photon/PhotonSyncTransformProxy.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+namespace Units.PureECS
+{
+    [Serializable]
+    public struct PhotonSyncTransform : IComponentData
+    {
+        public float3 networkPosition;
+        public quaternion networkRotation;
+        public float3 direction;
+        public double infoTime;
+    }
+
+    public class PhotonSyncTransformProxy : ComponentDataProxy<PhotonSyncTransform>, IPunObservable, IInRoomCallbacks
+    {
+        private float m_Distance;
+        private float m_Angle;
+
+        private UnitMainAI m_UnitMainAI;
+        private PhotonView m_PhotonView;
+        private Vector3 m_Direction;
+        private Vector3 m_NetworkPosition;
+        private Vector3 m_StoredPosition;
+
+        private Quaternion m_NetworkRotation;
+
+        public bool m_SynchronizePosition = true;
+        public bool m_SynchronizeRotation = true;
+
+        public void Awake()
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                return;
+#endif
+            m_PhotonView = GetComponent<PhotonView>();
+            m_UnitMainAI = GetComponent<UnitMainAI>();
+            m_StoredPosition = transform.position;
+            m_NetworkPosition = Vector3.zero;
+            m_NetworkRotation = Quaternion.identity;
+
+            
+            PhotonNetwork.AddCallbackTarget(this);
+        }
+        private void OnDestroy()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        private void Start()
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                return;
+#endif
+
+            if (!m_PhotonView.IsMine && m_UnitMainAI.enabled)
+            {
+                var entity = GetComponent<GameObjectEntity>();
+                entity.EntityManager.AddComponentData(entity.Entity, new Position() { Value = transform.position });
+                entity.EntityManager.AddComponentData(entity.Entity, new Rotation() { Value = transform.rotation });
+                entity.EntityManager.AddComponentData(entity.Entity, new CopyTransformToGameObject());
+
+                Value = new PhotonSyncTransform()
+                {
+                    networkPosition = transform.position,
+                    networkRotation = transform.rotation,
+                    direction = Vector3.zero,
+                    infoTime = PhotonNetwork.Time
+                };
+            }
+        }
+
+        public void OnMasterClientSwitched(Player newMasterClient)
+        {
+            if (m_PhotonView.IsMine && m_UnitMainAI.enabled)
+            {
+                var entity = GetComponent<GameObjectEntity>();
+                entity.EntityManager.RemoveComponent(entity.Entity, typeof(Position));
+                entity.EntityManager.RemoveComponent(entity.Entity, typeof(Rotation));
+                entity.EntityManager.RemoveComponent(entity.Entity, typeof(CopyTransformToGameObject));
+                entity.enabled = false;
+            }
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                if (this.m_SynchronizePosition)
+                {
+                    this.m_Direction = transform.position - this.m_StoredPosition;
+                    this.m_StoredPosition = transform.position;
+
+                    stream.SendNext(transform.position);
+                    stream.SendNext(this.m_Direction);
+                }
+
+                if (this.m_SynchronizeRotation)
+                {
+                    stream.SendNext(transform.rotation);
+                }
+            }
+            else
+            {
+                if (this.m_SynchronizePosition)
+                {
+                    this.m_NetworkPosition = (Vector3)stream.ReceiveNext();
+                    this.m_Direction = (Vector3)stream.ReceiveNext();
+                }
+
+                if (this.m_SynchronizeRotation)
+                {
+                    this.m_NetworkRotation = (Quaternion)stream.ReceiveNext();
+                }
+
+                Value = new PhotonSyncTransform()
+                {
+                    networkPosition = m_NetworkPosition,
+                    networkRotation = m_NetworkRotation,
+                    direction = m_Direction,
+                    infoTime = info.SentServerTime
+                };
+            }
+        }
+
+        public void OnPlayerEnteredRoom(Player newPlayer)
+        {
+        }
+
+        public void OnPlayerLeftRoom(Player otherPlayer)
+        {
+        }
+
+        public void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+        {
+        }
+
+        public void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+        {
+        }
+    }
+}
